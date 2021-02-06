0faa896a62a0999050cec582f38ef68fa19599b2
Tural <tural_m@hotmail.com>
2021-02-06 14:09:55 +0400

    create utils file,refactoring

--- initDb/initDb.go
+++ initDb/initDb.go
@@ -8,14 +8,14 @@ import (
 
 // DB is instance of *gorm.DB
 var DB *gorm.DB
-var err error
 
 // Init Config
 func init() {
 
-	DB, err = gorm.Open(mysql.Open(configs.DbConfig.DSN), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(configs.DbConfig.DSN), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
+	DB = db
 }
