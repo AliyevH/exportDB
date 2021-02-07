116a8ea66ada5305e94ad7eeec13cebec9724e2b
Hasan <hasan.aliyev.555@gmail.com>
2021-02-08 00:52:47 +0400

    first version ready

--- exporter/toCsv.go
+++ exporter/toCsv.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 	"sync"
 )
 
@@ -31,7 +33,53 @@ func ExportToCsv(path string, table []map[string]interface{}, tableName string, 
 	}
 
 	// Keys are id's. We will use this to access data in sorted by id order
-	csvData := PrepareData(table, columnNames)
+	keys := []int{}
+
+	// data is a map that will hold all data from table
+	data := map[int][]string{}
+
+	// row data is used for every iteration to hold data from that row
+	rowData := []string{}
+
+	for _, row := range table {
+		// Resetting rowData in every iteration to hold new row
+		rowData = nil
+
+		var id int
+
+		for _, columnName := range columnNames {
+
+			if columnName == "id" {
+				id = int(row["id"].(int32))
+				keys = append(keys, id)
+			} else {
+				switch row[columnName].(type) {
+				case int:
+					rowData = append(rowData, strconv.Itoa(row[columnName].(int)))
+				case int32:
+					tmp := int(row[columnName].(int32))
+					rowData = append(rowData, strconv.Itoa(tmp))
+				case string:
+					rowData = append(rowData, row[columnName].(string))
+				case nil:
+					rowData = append(rowData, "")
+				}
+			}
+
+		}
+		data[id] = rowData
+	}
+
+	sort.Ints(keys)
+
+	csvData := [][]string{}
+
+	for _, id := range keys {
+		d := []string{}
+		d = append(d, strconv.Itoa(id))
+		d = append(d, data[id]...)
+		csvData = append(csvData, d)
+	}
 
 	if err := w.WriteAll(csvData); err != nil {
 		log.Fatalln("error writing record to file", err)
